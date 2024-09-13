1765dfc2d7c83d6cd164b153e32afe9f5016d849
ben <beneslami72@gmail.com>
2024-09-13 10:57:21 +0000

    add level 3 high order markov chain

--- src/level3/Temporal_Locality.cpp
+++ src/level3/Temporal_Locality.cpp
@@ -1,7 +1,6 @@
 //
 // Created by ben on 3/30/24.
 //
-
 #include "Temporal_Locality.h"
 
 Temporal_Locality::Temporal_Locality() {
@@ -12,11 +11,11 @@ Temporal_Locality::~Temporal_Locality() {
 
 }
 
-void Temporal_Locality::set_iat(MarkovChain *dist) {
+void Temporal_Locality::set_iat(RandomGenerator::CustomDistribution *dist) {
     this->iat = dist;
 }
 
-void Temporal_Locality::set_burst_duration(MarkovChain *dist) {
+void Temporal_Locality::set_burst_duration(RandomGenerator::CustomDistribution *dist) {
     this->burst_duration = dist;
 }
 
@@ -25,11 +24,11 @@ void Temporal_Locality::set_burst_volume(std::map<int, RandomGenerator::CustomDi
 }
 
 int Temporal_Locality::generate_iat() {
-    return this->iat->get_next_state();
+    return this->iat->Generate();
 }
 
 int Temporal_Locality::generate_burst_duration() {
-    return this->burst_duration->get_next_state();
+    return this->burst_duration->Generate();
 }
 
 int Temporal_Locality::generate_burst_volume(int duration) {
@@ -38,9 +37,9 @@ int Temporal_Locality::generate_burst_volume(int duration) {
 
 void Temporal_Locality::show_model(){
     std::cout << "----Inter Arrival Time----\n";
-    this->iat->show();
+    this->iat->show_cdf();
     std::cout << "----Burst Duration----\n";
-    this->burst_duration->show();
+    this->burst_duration->show_cdf();
     std::map<int, RandomGenerator::CustomDistribution*>::iterator it;
     for(it = burst_volume.begin(); it != burst_volume.end(); ++it) {
         std::cout << "----Burst volume " << it->first << "----\n";
